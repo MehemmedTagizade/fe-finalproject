c32fbe5a49fcd56212cd1fb1f645127f0ee3c397
Your Name <you@example.com>
2021-09-02 01:08:40 +0400

    9

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,79 @@
-const tabNavigations = Array.from(document.querySelectorAll(".bttn"));
+// blog//
+// const tabNavigations = Array.from(document.querySelectorAll(".bttn"));
 
 
-const tabContents = Array.from(document.querySelectorAll("cards_blog .blog_part"));
+// const tabContents = Array.from(document.querySelectorAll(".cards_blog .blog_part"));
 
-const clearActives = () => {
-    tabNavigations.forEach(tabNavigation => {
-        tabNavigation.classList.remove("active");
-    });
+// const clearActives = () => {
+//     tabNavigations.forEach(tabNavigation => {
+//         tabNavigation.classList.remove("active2");
+//     });
 
-    tabContents.forEach(tabContent => {
-        tabContent.classList.remove("active1");
-    });
+//     tabContents.forEach(tabContent => {
+//         tabContent.classList.remove("active1");
+//     });
+
+
+// }
+
+
+// tabNavigations.forEach(tabNavigation => {
+//     tabNavigation.onclick = () => {
+//         clearActives();
+//         event.preventDefault();
+//         const targetId = tabNavigation.getAttribute("data_bttn_a");
+//         const targetContent = document.getElementById(targetId)
+//         tabNavigation.classList.add("active2");
+//         targetContent.classList.add("active1")
+
+//     }
+// });
+
+//blog end///
+
+//nav-burger//
+// const navslide = () => {
+//     const burger = document.querySelector(".nav .sd .burger_svg");
+//     const rightSide = document.querySelector(".nav .sd .right_side");
+
+//     burger.onclick = () => {
+//         rightSide.classList.toggle("activeburger")
+//     }
+
+// }
+// const divv = document.querySelector("div")
+
+// burger.addEventListener('click', () => {
+//     divv.style.color = "red"
+
+
+// }
+// )
+const burger = document.querySelector(".nav .sd .burger_svg");
+const rightSide = document.querySelector(".nav .sd .right_side");
+
+burger.onclick = () => {
+    // rightSide.classList.toggle("activeburger");
+    rightSide.classList.toggle("activeburger")
 }
 
+// navslide();
+// burger.addEventListener("click", function () {
 
-tabNavigations.forEach(tabNavigation => {
-    tabNavigation.onclick = () => {
-        clearActives();
+// })
 
-        const targetId = tabNavigation.getAttribute("data_bttn_a");
-        const targetContent = document.getElementById(targetId)
-        tabNavigation.classList.add("active");
-        targetContent.classList.add("active1")
 
-    }
-});
+
+
+
+
+
+
+
+
+
+
+
+//nav-burger-end//
+
 
